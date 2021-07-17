675c29bbcac52e6b58f5d86a247a4b55d8157b09
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-07-17 18:53:11 +0100

    Adds message if no results are found

--- assets/js/list.js
+++ assets/js/list.js
@@ -2,4 +2,9 @@ var options = {
     valueNames: ['surname', 'forenames', 'name'],
 };
 
-var graveList = new List('graveList', options);
+var graveList = new List('graveList', options)
+    .on('updated', function(list) {
+        if (list.matchingItems.length == 0) {
+          list.list.innerHTML = '<tr style="text-align:center"><td colspan="3">No items found</td></tr>';
+        }
+    });
